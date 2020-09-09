d32cb9c932446f96f667c90113b4da59c87f63a6
Habib Ur Rehman <hbrehman98@gmail.com>
2020-09-09 14:56:03 +0500

    learned insertion sort

--- insertionSrot.js
+++ insertionSrot.js
@@ -1,13 +1,14 @@
 (() => {
   function insertionSort(arr) {
-    for (let i = 1; i < arr.length; i++) {
-      for (let j = 0; j < i; j++) {
-        if (arr[j] < arr[i]) {
-          //
-        }
+    var currentVal;
+    for (var i = 1; i < arr.length; i++) {
+      currentVal = arr[i];
+      for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+        arr[j + 1] = arr[j];
       }
+      arr[j + 1] = currentVal;
     }
+    return arr;
   }
+  console.log(insertionSort([2, 1, 9, 76, 4]));
 })();
-
-// this is called and iify(immediately invoked function expression)
