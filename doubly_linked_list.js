c48af3fa7bd47485eecfbba0490174aa464b97f2
Habib Ur Rehman <hbrehman98@gmail.com>
2020-10-02 23:23:43 +0500

    implemented push pop for doubly linked list

---
+++ doubly_linked_list.js
@@ -0,0 +1,71 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+class DLL {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+    }
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.head) return null;
+    const removed = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = removed.prev;
+      this.tail.next = null;
+      removed.prev = null;
+    }
+    this.length--;
+    return removed;
+  }
+
+  shift() {
+    if (!this.head) return null;
+    const removed = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = removed.next;
+      this.head.prev = null;
+      removed.next = null;
+    }
+    this.length--;
+    return removed;
+  }
+
+  unshift(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+const dll = new DLL();
